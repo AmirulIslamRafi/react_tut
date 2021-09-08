8fa775361b0b9583ec6524fbb279c8db875f5972
Amirul <rafikhan97ctg@gmail.com>
2021-09-08 17:29:10 +0600

    adding bootstrap/ react props

---
+++ harrybhai.mjs
@@ -0,0 +1,11 @@
+const a = "harry Bhai";
+const b = "W3 school";
+const c = "React Doc";
+const d = "Thapa Technical";
+const e = "Node/express/Nginx";
+
+export default (a);
+export {b};
+export {c};
+export {d};
+export {e};
---
+++ thapatec.mjs
@@ -0,0 +1,6 @@
+import first, {b, c, d, e} from './harrybhai.mjs'
+
+console.log(first);
+console.log(d);
+console.log(e);
+console.log(c);
